245a68945e9cc3874336e5d7e923aec870f27e1f
TheFear36 <gpodvorotov@gmail.com>
2021-09-07 22:49:11 +0300

    Homework 9

---
+++ src/lesson9/MyExceprion/MyArrayDataException.java
@@ -0,0 +1,11 @@
+package lesson9.MyExceprion;
+
+public class MyArrayDataException extends Exception {
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+
+    public MyArrayDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
